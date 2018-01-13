5d87fac1ca51d141ea80b868a8f2a9110e4c2bc5
Kevin Chen <kchen3568@gmail.com>
2018-01-12 21:46:38 -0600

    realize need actual solution

--- logic.py
+++ logic.py
@@ -1,11 +1,43 @@
-def doTwo(num1, num2):
+#considered operations include - 
+#addition (a+b), subtraction (a-b), multiplication (axb), division (a/b), exponentiation (a^b), 
+#logarithms (log_a(b)), concatenation (ab), factorials (a!)
+
+
+def doTwo(num1, num2): #return two lists - one for numbers, one for solutions to those numbers
+	nums1 = num1[0]
+	nums2 = num2[0]
+	sols1 = num1[1]
+	sols2 = num2[1]
+
 	pass
 
 def doListTwo(results,list1,list2):
+	nums1 = list1[0]
+	nums2 = list2[0]
+	sols1 = list1[1]
+	sols2 = list2[1]
+
 	pass
 
-def doThree(num1, num2, num3):
+def doThree(num1, num2, num3): #return two lists - one for numbers, one for solutions to those numbers
+	nums1 = num1[0]
+	nums2 = num2[0]
+	nums3 = num3[0]
+	sols1 = num1[1]
+	sols2 = num2[1]
+	sols3 = num3[1]
+
 	pass
 
 def doListThree(results, l, num):
+	nums1 = l[0]
+	nums2 = num[0]
+	sols1 = l[1]
+	sols2 = num[1]
+
 	pass
+
+factorial = [0]*100
+factorial[0] = 1
+for i in range(1,100):
+	factorial[i] = factorial[i-1]*i
--- wgyn.py
+++ wgyn.py
@@ -3,10 +3,10 @@ from . import logic
 
 if __name__ == "__main__":
 	args = sorted([sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]])
-	num1 = args[0]
-	num2 = args[1]
-	num3 = args[2]
-	num4 = args[3]
+	num1 = [int(args(0)),args[0]]
+	num2 = [int(args(1)),args[1]]
+	num3 = [int(args(2)),args[2]]
+	num4 = [int(args(3)),args[3]]
 
 def wgyn(num1, num2, num3, num4):
 	results = [[] for i in range(100)] #array for solutions for each number
@@ -21,12 +21,12 @@ def twoByTwo(results, num1, num2, num3, num4):
 	list2 = doTwo(num3,num4)
 	doListTwo(results,list1,list2)
 
-	if num2 != num3: #don't need to calculate again if there are duplicates
+	if num2[0] != num3[0]: #don't need to calculate again if there are duplicates
 		list1 = doTwo(num1,num3)
 		list2 = doTwo(num2,num4)
 		doListTwo(results,list1,list2)
 
-	if num2 != num4:
+	if num2[0] != num4[0]:
 		list1 = doTwo(num1,num4)
 		list2 = doTwo(num2,num3)
 		doListTwo(results,list1,list2)
@@ -36,15 +36,15 @@ def threeByOne(results, num1, num2, num3, num4):
 	l = doThree(num1,num2,num3)
 	doListThree(results,l,num4)
 
-	if num1 != num4:
+	if num1[0] != num4[0]:
 		l = doThree(num2,num3,num4)
 		doListThree(results,l,num1)
 
-	if num2 != num4:
+	if num2[0] != num4[0]:
 		l = doThree(num1,num3,num4)
 		doListThree(results,l,num2)
 
-	if num3 != num4:
+	if num3[0] != num4[0]:
 		l = doThree(num1,num2,num4)
 		doListThree(results,l,num3)
 
