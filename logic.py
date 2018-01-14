5a0799f45dd11dc74b6e6db0ac8edbaaacd869f5
Kevin Chen <kchen3568@gmail.com>
2018-01-13 20:34:10 -0600

    mostly done

--- logic.py
+++ logic.py
@@ -100,19 +100,21 @@ def doTwo(num1, num2): #return two lists - one for numbers, one for solutions to
 	
 	#concatenation
 	if nums1 != 0:
-		current = nums1*10+nums2
-		if current not in generated[0]:
-			solfact = sols1+sols2
-			generated[0].append(current)
-			generated[1].append(solfact)
-			applyFact(generated,current,solfact)
+		if '!' not in sols1 and '!' not in sols2: #cannot concatenate numbers if they are the result of a factorial
+			current = nums1*10+nums2
+			if current not in generated[0]:
+				solfact = sols1+sols2
+				generated[0].append(current)
+				generated[1].append(solfact)
+				applyFact(generated,current,solfact)
 	if nums2 != 0:
-		current = nums2*10+nums1
-		if current not in generated[0]:
-			solfact = sols2+sols1
-			generated[0].append(current)
-			generated[1].append(solfact)
-			applyFact(generated,current,solfact)
+		if '!' not in sols1 and '!' not in sols2:
+			current = nums2*10+nums1
+			if current not in generated[0]:
+				solfact = sols2+sols1
+				generated[0].append(current)
+				generated[1].append(solfact)
+				applyFact(generated,current,solfact)
 
 	return generated
 
@@ -153,18 +155,24 @@ def doListTwo(results,list1,list2):
 				finalApplyFact(results,current,solfact)
 
 			#division
-			current = float(nums1[i])/nums2[j]
-			if int(current) == current and current > 0 and current <= 100:
-				current = int(current)
-				solfact = '('+sols1[i]+')/('+sols2[j]+')'
-				results[current-1].append(solfact)
-				finalApplyFact(results,current,solfact)
-			current = float(nums2[j])/nums1[i]
-			if int(current) == current and current > 0 and current <= 100:
-				current = int(current)
-				solfact = '('+sols2[j]+')/('+sols1[i]+')'
-				results[current-1].append(solfact)
-				finalApplyFact(results,current,solfact)
+			try: #division by zero
+				current = float(nums1[i])/nums2[j]
+				if int(current) == current and current > 0 and current <= 100:
+					current = int(current)
+					solfact = '('+sols1[i]+')/('+sols2[j]+')'
+					results[current-1].append(solfact)
+					finalApplyFact(results,current,solfact)
+			except Exception as e:
+				pass
+			try:
+				current = float(nums2[j])/nums1[i]
+				if int(current) == current and current > 0 and current <= 100:
+					current = int(current)
+					solfact = '('+sols2[j]+')/('+sols1[i]+')'
+					results[current-1].append(solfact)
+					finalApplyFact(results,current,solfact)
+			except Exception as e:
+				pass
 
 			#exponentiation
 			try: #math overflow error
@@ -205,29 +213,181 @@ def doListTwo(results,list1,list2):
 					finalApplyFact(results,current,solfact)
 			except Exception as e:
 				pass
-			
 
+def doThree(results, l, finalNum):
+	num1 = l[0]
+	num2 = l[1]
+	num3 = l[2]
 
-def doThree(num1, num2, num3): #return two lists - one for numbers, one for solutions to those numbers
-	nums1 = num1[0]
-	nums2 = num2[0]
-	nums3 = num3[0]
-	sols1 = num1[1]
-	sols2 = num2[1]
-	sols3 = num3[1]
+	doneTwo = doTwo(num1,num2)
+	doneTwoNowThree(results,doneTwo,num3,finalNum)
+	if num2 != num3:
+		doneTwo = doTwo(num1,num3)
+		doneTwoNowThree(results,doneTwo,num2,finalNum)
+	if num1 != num2:
+		doneTwo = doTwo(num2,num3)
+		doneTwoNowThree(results,doneTwo,num1,finalNum)
 
-	pass
+	#triple concatenation then operation, 6 for each permutation of concatenation
+	#if finalNum is a duplicate of one of the other 3, there may be duplicates in 3x1s
+	isFactorial = False # check if any of the numbers being concatenated are the result of a factorial
+	for i in range(len(l)):
+		if '!' in l[i][1]:
+			isFactorial = True
+	if not isFactorial:
+		concat = [num1[0]*100+num2[0]*10+num3[0],num1[1]+num2[1]+num3[1]]
+		doConcat(results,concat,finalNum)
+		if num2[0] != num3[0]:
+			concat = [num1[0]*100+num3[0]*10+num2[0],num1[1]+num3[1]+num2[1]]
+			doConcat(results,concat,finalNum)
+		if num1[0] != num2[0]:
+			concat = [num2[0]*100+num1[0]*10+num3[0],num2[1]+num1[1]+num3[1]]
+			doConcat(results,concat,finalNum)
+			if num1[0] != num3[0]:
+				concat = [num2[0]*100+num3[0]*10+num1[0],num2[1]+num3[1]+num1[1]]
+				doConcat(results,concat,finalNum)
+		if num3[0] != num1[0]:
+			concat = [num3[0]*100+num1[0]*10+num2[0],num3[1]+num1[1]+num2[1]]
+			doConcat(results,concat,finalNum)
+			if num1[0] != num2[0]:
+				concat = [num3[0]*100+num2[0]*10+num1[0],num3[1]+num2[1],num1[1]]
+				doConcat(results,concat,finalNum)
 
-def doListThree(results, l, num):
-	nums1 = l[0]
-	nums2 = num[0]
-	sols1 = l[1]
-	sols2 = num[1]
+def doneTwoNowThree(results, doneTwo, num3, finalNum):
+	dtNums = doneTwo[0]
+	dtSols = doneTwo[1]
+	if finalNum[0] > 2 and int(finalNum[0]) == finalNum[0]:
+		finalNumIterate = int(finalNum[0])
+		finalSolIterate = finalNum[1]
+		while finalNumIterate < 20:
+			finalNumIterate = factorial[finalNumIterate]
+			finalSolIterate = '('+finalSolIterate+')!'
+			finalNum.append(finalNumIterate)
+			finalNum.append(finalSolIterate)
 
-	pass
+	for i in range(len(dtNums)):
+
+		#operation for finalNum is addition or subtraction, accounts for other last operations if there is overlap
+		current = dtNums[i]*num3[0]
+		solfact = '('+dtSols[i]+')*'+num3[1]
+		if current > 2 and current <= 20: #whether it should consider a factorial
+			applyFinalNum(results,current,solfact,finalNum,4)
+		else:
+			applyFinalNum(results,current,solfact,finalNum,0)
+			applyFinalNum(results,current,solfact,finalNum,2)
+			applyFinalNum(results,current,solfact,finalNum,3)
+		
+		try:
+			current = dtNums[i]/num3[0]
+			solfact = '('+dtSols+')/'+num3[1]
+			if current > 2 and current <= 20:
+				applyFinalNum(results,current,solfact,finalNum,4)
+			else:
+				applyFinalNum(results,current,solfact,finalNum,0)
+				applyFinalNum(results,current,solfact,finalNum,2)
+				applyFinalNum(results,current,solfact,finalNum,3)
+		except Exception as e:
+			pass
+
+		try:
+			current = math.pow(dtNums[i],num3[0])
+			solfact = '('+dtSols+')^'+num3[1]
+			if current > 2 and current <= 20:
+				applyFinalNum(results,current,solfact,finalNum,4)
+			else:
+				applyFinalNum(results,current,solfact,finalNum,0)
+				applyFinalNum(results,current,solfact,finalNum,1)
+				applyFinalNum(results,current,solfact,finalNum,3)
+		except Exception as e:
+			pass
+
+		try:
+			current = math.pow(num3[0],dtNums[i])
+			solfact = num3[1]+'^('+dtNums+')'
+			if current > 2 and current <= 20:
+				applyFinalNum(results,current,solfact,finalNum,4)
+			else:
+				applyFinalNum(results,current,solfact,finalNum,0)
+				applyFinalNum(results,current,solfact,finalNum,1)
+				applyFinalNum(results,current,solfact,finalNum,3)
+		except Exception as e:
+			pass
+
+		try:
+			current = math.log(dtNums[i],num3[0])
+			solfact = 'log_('+dtSols+')('+num3[1]+')'
+			if current > 2 and current <= 20:
+				applyFinalNum(results,current,solfact,finalNum,4)
+			else:
+				applyFinalNum(results,current,solfact,finalNum,0)
+				applyFinalNum(results,current,solfact,finalNum,1)
+				applyFinalNum(results,current,solfact,finalNum,2)
+		except Exception as e:
+			pass
+
+		try:
+			current = math.log(num3[0],dtNums[i])
+			solfact = 'log_'+num3[1]+'('+dtSols[i]+')'
+			if current > 2 and current <= 20:
+				applyFinalNum(results,current,solfact,finalNum,4)
+			else:
+				applyFinalNum(results,current,solfact,finalNum,0)
+				applyFinalNum(results,current,solfact,finalNum,1)
+				applyFinalNum(results,current,solfact,finalNum,2)
+		except Exception as e:
+			pass
+
+		#operation for finalNum is multiplication or division
+		current = dtNums[i]+num3[0]
+		solfact = dtSols[i]+'+'+num3[1]
+		if current > 2 and current <= 20:
+			applyFinalNum(results,current,solfact,finalNum,4)
+		else:
+			applyFinalNum(results,current,solfact,finalNum,1)
+			applyFinalNum(results,current,solfact,finalNum,2)
+			applyFinalNum(results,current,solfact,finalNum,3)
+
+		if dtNums[i] > num3[0]:
+			current = dtNums[i]-num3[0]
+			solfact = dtSols[i]+'-'+num3[1]
+			if current > 2 and current <= 20:
+				applyFinalNum(results,current,solfact,finalNum,4)
+			else:
+				applyFinalNum(results,current,solfact,finalNum,1)
+				applyFinalNum(results,current,solfact,finalNum,2)
+				applyFinalNum(results,current,solfact,finalNum,3)
+		else:
+			current = num3[0]-dtNums[i]
+			solfact = num3[1]+'-('+dtSols[i]+')'
+			if current > 2 and current <= 20:
+				applyFinalNum(results,current,solfact,finalNum,4)
+			else:
+				applyFinalNum(results,current,solfact,finalNum,1)
+				applyFinalNum(results,current,solfact,finalNum,2)
+				applyFinalNum(results,current,solfact,finalNum,3)
+
+def doConcat(results,concat,finalNum):
+	current = concat[0]-finalNum[0]
+	if current > 0 and current <= 100:
+		results[current-1].append(concat[1]+'-'+finalNum[1])
+	try:
+		current = concat[0]/finalNum[0]
+		if int(current) == current and current > 0 and current <= 100:
+			current = int(current)
+			results[current-1].append(concat[1]+'/'+finalNum[1])
+	except Exception as e:
+		pass
+	try:
+		current = math.log(concat[0],finalNum[0])
+		if int(current) == current and current > 0 and current <= 100:
+			current = int(current)
+			results[current-1].append('log_'+finalNum[1]+'('+concat[1]+')')
+	except Exception as e:
+		pass
 
 def applyFact(generated, current, solfact): #continuously adds to generated set of numbers by doing a factorial on the result under a certain threshold (20)
 	if current > 2 and int(current) == current:
+		current = int(current)
 		while current <= 20:
 			solfact = '('+solfact+')!'
 			if factorial[current] not in generated[0]:
@@ -237,9 +397,99 @@ def applyFact(generated, current, solfact): #continuously adds to generated set 
 
 def finalApplyFact(results, current, solfact):
 	if current == 3 or current == 4:
+		current = int(current)
 		current = factorial[current]
 		solfact = '('+solfact+')!'
 		results[current-1].append(solfact)
+
+def applyFinalNum(results, current, solfact, finalNum, operation): #value of operation is index in [+ and -, x and /, ^, log, !] to figure out the final operation to be made
+	for i in range(len(finalNum)//2):
+		if operation == 4:
+			while int(current) == current and current > 2 and current <= 20:
+				applyFinalNum(results,current,solfact,finalNum,0)
+				applyFinalNum(results,current,solfact,finalNum,1)
+				applyFinalNum(results,current,solfact,finalNum,2)
+				applyFinalNum(results,current,solfact,finalNum,3)
+				current = int(current)
+				current = factorial[current]
+				solfact = '('+solfact+')!'
+
+		elif operation == 0:
+			finalCurrent = current+finalNum[2*i]
+			if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+				finalCurrent = int(finalCurrent)
+				results[finalCurrent-1].append(solfact+'+'+finalNum[2*i+1])
+			finalCurrent = current-finalNum[2*i]
+			if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+				finalCurrent = int(finalCurrent)
+				results[finalCurrent-1].append(solfact+'-'+finalNum[2*i+1])
+			finalCurrent = finalNum[2*i]-current
+			if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+				finalCurrent = int(finalCurrent)
+				results[finalCurrent-1].append(finalNum[2*i+1]+'-('+solfact+')')
+
+		elif operation == 1:
+			finalCurrent = current*finalNum[2*i]
+			if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+				finalCurrent = int(finalCurrent)
+				results[finalCurrent-1].append('('+solfact+')*'+finalNum[2*i+1])
+			try:
+				finalCurrent = current/finalNum[2*i]
+				if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+					finalCurrent = int(finalCurrent)
+					results[finalCurrent-1].append('('+solfact+')/'+finalNum[2*i+1])
+			except Exception as e:
+				pass
+			try:
+				finalCurrent = finalNum[2*i]/current
+				if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+					finalCurrent = int(finalCurrent)
+					results[finalCurrent-1].append(finalNum[2*i+1]+'/('+solfact+')')
+			except Exception as e:
+				pass
+
+		elif operation == 2:
+			try:
+				finalCurrent = math.pow(current,finalNum[2*i])
+				if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+					finalCurrent = int(finalCurrent)
+					results[finalCurrent-1].append('('+solfact+')^'+finalNum[2*i+1])
+			except Exception as e:
+				pass
+			try:
+				finalCurrent = math.pow(finalNum[2*i],current)
+				if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+					finalCurrent = int(finalCurrent)
+					results[finalCurrent-1].append(finalNum[2*i+1]+'^('+solfact+')')
+			except Exception as e:
+				pass
+
+		elif operation == 3:
+			try:
+				finalCurrent = math.log(current,finalNum[2*i])
+				if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+					finalCurrent = int(finalCurrent)
+					results[finalCurrent-1].append('log_('+solfact+')('+finalNum[2*i+1]+')')
+			except Exception as e:
+				pass
+			try:
+				finalCurrent = math.log(finalNum[2*i],solfact)
+				if int(finalCurrent) == finalCurrent and finalCurrent > 0 and finalCurrent <= 100:
+					finalCurrent = int(finalCurrent)
+					results[finalCurrent-1].append('log_'+finalNum[2*i+1]+'('+solfact+')')
+			except Exception as e:
+				pass
+			
+def findFactCombos(num):
+	val = num[0]
+	combos = [val]
+	combosSol = [num[1]]
+	if val > 2:
+		while val < 20:
+			val = factorial[val]
+			combos.append(val)
+			combosSol.append('('+combosSol[len(combosSol)-1]+')!')
+	return [combos, combosSol]
 
 
 factorial = [0]*21 #stores 0! to 99!
@@ -247,19 +497,19 @@ factorial[0] = 1
 for i in range(1,21):
 	factorial[i] = factorial[i-1]*i
 
-l1 = [4,'4']
-l2 = [5,'5']
-l3 = [6,'6']
-l4 = [7,'7']
-results = [[] for i in range(100)]
+# l1 = [4,'4']
+# l2 = [5,'5']
+# l3 = [6,'6']
+# l4 = [7,'7']
+# results = [[] for i in range(100)]
 
-first = doTwo(l1,l2)
-second = doTwo(l3,l4)
-doListTwo(results,first,second)
-count = 0
-for i in range(100):
-	print(str(i+1)+':')
-	print(results[i])
-	if len(results[i]) != 0:
-		count += 1
-print('count: ' + str(count))
+# first = doTwo(l1,l2)
+# second = doTwo(l3,l4)
+# doListTwo(results,first,second)
+# count = 0
+# for i in range(100):
+# 	print(str(i+1)+':')
+# 	print(results[i])
+# 	if len(results[i]) != 0:
+# 		count += 1
+# print('count: ' + str(count))
--- wgyn.py
+++ wgyn.py
@@ -1,17 +1,39 @@
 import sys
-from . import logic
+from logic import *
 
 if __name__ == "__main__":
 	args = sorted([sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]])
-	num1 = [int(args(0)),args[0]]
-	num2 = [int(args(1)),args[1]]
-	num3 = [int(args(2)),args[2]]
-	num4 = [int(args(3)),args[3]]
+	num1 = [int(args[0]),args[0]]
+	num2 = [int(args[1]),args[1]]
+	num3 = [int(args[2]),args[2]]
+	num4 = [int(args[3]),args[3]]
 
 def wgyn(num1, num2, num3, num4):
 	results = [[] for i in range(100)] #array for solutions for each number
-	twoByTwo(results,num1,num2,num3,num4)
-	threeByOne(results,num1,num2,num3,num4)
+
+	num1 = findFactCombos(num1) #adds factorials to the set of original numbers
+	num2 = findFactCombos(num2)
+	num3 = findFactCombos(num3)
+	num4 = findFactCombos(num4)
+
+	for i in range(len(num1[0])): #iterates through all combinations of original numbers and factorials of original numbers
+		for j in range(len(num2[0])):
+			for k in range(len(num3[0])):
+				for l in range(len(num4[0])):
+					twoByTwo(results,
+						[num1[0][i],num1[1][i]],
+						[num2[0][j],num2[1][j]],
+						[num3[0][k],num3[1][k]],
+						[num4[0][l],num4[1][l]],)
+	for i in range(len(num1[0])):
+		for j in range(len(num2[0])):
+			for k in range(len(num3[0])):
+				for l in range(len(num4[0])):
+					threeByOne(results,
+						[num1[0][i],num1[1][i]],
+						[num2[0][j],num2[1][j]],
+						[num3[0][k],num3[1][k]],
+						[num4[0][l],num4[1][l]],)
 	return results
 
 
@@ -33,19 +55,27 @@ def twoByTwo(results, num1, num2, num3, num4):
 
 #obtain solutions by dividing the set of numbers into a set of three and a set of one, then combining numbers made from those sets
 def threeByOne(results, num1, num2, num3, num4):
-	l = doThree(num1,num2,num3)
-	doListThree(results,l,num4)
+	l = [num1,num2,num3]
+	doThree(results,l,num4)
 
 	if num1[0] != num4[0]:
-		l = doThree(num2,num3,num4)
-		doListThree(results,l,num1)
+		l = [num2,num3,num4]
+		doThree(results,l,num1)
 
 	if num2[0] != num4[0]:
-		l = doThree(num1,num3,num4)
-		doListThree(results,l,num2)
+		l = [num1,num3,num4]
+		doThree(results,l,num2)
 
 	if num3[0] != num4[0]:
-		l = doThree(num1,num2,num4)
-		doListThree(results,l,num3)
+		l = [num1,num2,num4]
+		doThree(results,l,num3)
 
-print(wgyn(num1,num2,num3,num4))
+results = wgyn(num1,num2,num3,num4)
+count = 0
+for i in range(len(results)):
+	print(str(i+1)+': ')
+	print(results[i])
+	print('-------------------------------------------------------------')
+	if len(results[i]) != 0:
+		count += 1
+print('solved: '+str(count))
